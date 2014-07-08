b47710bf485d955d2b75ef2277dda3b94bc8c0b1
kkanok <kkanok@kkanok-Aspire-V3-551G.(none)>
2014-07-08 18:39:36 +0300

    1st form test

--- src/GoyGoyEdu/PortalBundle/Controller/DefaultController.php
+++ src/GoyGoyEdu/PortalBundle/Controller/DefaultController.php
@@ -2,12 +2,35 @@
 
 namespace GoyGoyEdu\PortalBundle\Controller;
 
+use GoyGoyEdu\PortalBundle\Form\PersonType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use GoyGoyEdu\PortalBundle\Entity\Person;
+use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
     public function indexAction($name)
     {
         return $this->render('GoyGoyEduPortalBundle:Default:index.html.twig', array('name' => $name));
+    }
+    public function homeAction(Request $request)
+    {
+     $entity = new Person();
+     $form = new PersonType();
+     $data = $this->createFormBuilder($entity);
+     $form->buildForm($data,[]);
+     $form = $data->getForm();
+     $form->handleRequest($request);
+     if ($form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($entity);
+            $em->flush();
+            //return $this->redirect($this->generateUrl('journal_show', array('id' => $entity->getId())));
+        }
+     
+       return $this->render('GoyGoyEduPortalBundle:Default:index.html.twig', 
+               ["form" =>  $form->createView() ]);
+        
+        //eturn $this->SignUpForm();
     }
 }
---
+++ src/GoyGoyEdu/PortalBundle/Form/PersonType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace GoyGoyEdu\PortalBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolverInterface;
+
+class PersonType extends AbstractType
+{
+        /**
+     * @param FormBuilderInterface $builder
+     * @param array $options
+     */
+    public function buildForm(FormBuilderInterface $builder, array $options) 
+    {
+        $builder
+            ->add('name','text')
+            ->add('surname','text')
+            ->add('email','email')            
+            ->add('kaydet','submit')
+
+        ;
+    }
+    
+    /**
+     * @param OptionsResolverInterface $resolver
+     */
+    public function setDefaultOptions(OptionsResolverInterface $resolver)
+    {
+        $resolver->setDefaults(array(
+            'data_class' => 'GoyGoyEdu\PortalBundle\Entity\Person'
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return 'goygoyedu_portalbundle_person';
+    }
+}
