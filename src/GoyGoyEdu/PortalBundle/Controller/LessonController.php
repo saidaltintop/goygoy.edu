d1b1f89d531879adaab18962ac46ae9373fe29bd
kkanok <kkanok@kkanok-Aspire-V3-551G.(none)>
2014-07-10 16:00:28 +0300

    lesson teacher assigment added

--- src/GoyGoyEdu/PortalBundle/Controller/LessonController.php
+++ src/GoyGoyEdu/PortalBundle/Controller/LessonController.php
@@ -47,15 +47,145 @@ class LessonController extends Roles {
         }
         return null;
     }
+    function getTerms($key)
+    {
+        $dep = $this->getDepartment($key);
+        $data = $this->getDoctrine()->getRepository("GoyGoyEduPortalBundle:Term")
+                ->findBy(["department"=>$dep]);
+        if(is_array($data))
+        {
+            $result = [];
+            foreach ($data as $value) {
+                if($value -> getType() == 1)
+                {
+                    $result[]  = [$value->getId() => $value->getYear()->format("Y") . " Bahar" ];
+
+                }
+                else
+                {
+                    $result[]  = [$value->getId() => $value->getYear()->format("Y") . "Güz" ];
+
+                }
+            }
+            return $result;
+        }
+        return [];
+       // var_dump($data);
+    }
+    function getFaculty($key)
+    {
+          return  $this->getDoctrine()->getRepository("GoyGoyEduPortalBundle:Faculty")
+                ->find($key);
+    }
+    function getTeachers()
+    {
+        $role = $this->getDoctrine()->getRepository("GoyGoyEduPortalBundle:Role")
+                ->find(5);
+        if(is_object($role))
+        {
+            $peopleids = $this->getDoctrine()->getRepository("GoyGoyEduPortalBundle:PersonHasRole")
+                ->findBy(["role"=>  $role]);
+            $result = [];
+            foreach ($peopleids as $value) {
+                $result[] = [ $value->getPerson()->getId() => $value->getPerson()->getName() . " " .$value->getPerson()->getSurname() ];
+            }
+            return $result;
+        }
+        //var_dump($role);
+    }
+    function getLessons()
+    {
+        $lessons = $this->getDoctrine()->getRepository("GoyGoyEduPortalBundle:Lesson")
+                ->findAll();
+        $result = [];
+        foreach ($lessons as $value) {
+        $result[] = [$value->getId() => $value->getName()];
+        }
+        return $result;
+    }
+    function getLesson($key)
+    {
+        return  $this->getDoctrine()->getRepository("GoyGoyEduPortalBundle:Lesson")
+                ->find($key);
+    }
+    function getTerm($key)
+    {
+        return  $this->getDoctrine()->getRepository("GoyGoyEduPortalBundle:Term")
+                ->find($key);
+    }
+    function getTeacher($key)
+    {
+          return  $this->getDoctrine()->getRepository("GoyGoyEduPortalBundle:Person")
+                ->find($key);
+    }
     function newtermAction(\Symfony\Component\HttpFoundation\Request $request , $key = 2) {
         if(!$this->isValid(4))
         {
             echo "no access";
             return new \Symfony\Component\HttpFoundation\Response;
         }
+        $task = new \GoyGoyEdu\PortalBundle\Entity\PersonToLesson();
+        $first = $this->getFaculties();
+        $count = 0;
+        foreach ($first[0] as $k => $value) {
+            if($count == 0)
+            {
+                $first = $k;
+                break;
+            }
+            $count++;
+        }
+        $form = $this->createFormBuilder($task,['csrf_protection' => false])
         
+        ->add('faculties', 'choice', [
+                'choices'   => $this->getFaculties(),
+                'mapped'=>false
+            ])
+         ->add('department', 'choice', [
+                'choices'   => $this->getDepartments($first),
+                'mapped'=>false
+            ])
+         ->add('terms', 'choice', [
+                'choices'   => $this->getTerms($key),
+                'mapped'=>false
+            ])
+         ->add('teachers', 'choice', [
+                'choices'   => $this->getTeachers(),
+                'mapped'=>false
+            ])
+          ->add('lessons', 'choice', [
+                'choices'   => $this->getLessons(),
+                'mapped'=>false
+            ])
+         ->add('credit')
+         ->add('save','submit')
+
+            ->getForm();
+        $form->handleRequest($request);
+        if($form->isValid())
+        {
+            $faculty = $this->getFaculty($_POST["form"]["faculties"]);
+            $department = $this->getDepartment($_POST["form"]["department"]);
+            $term = $this->getTerm($_POST["form"]["terms"]);
+            $teacher = $this->getTeacher($_POST["form"]["teachers"]);
+            $lesson = $this->getLesson($_POST["form"]["lessons"]);
+            $task->setDepartment($department);
+            $task->setLesson($lesson);
+            $task->setPerson($teacher);
+            $task->setTerm($term);
+            $task->setType(1);
+//            $department = $this->getDepartment($_POST["form"]["department"]);
+//            $task->setDepartment($department);
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($task);
+            $em->flush();
+            echo "added";
+        }
+        return $this->render('GoyGoyEduPortalBundle:Portal:insert.html.twig', 
+               ["form" =>  $form->createView()  ]);
+    
     }
-    function newAction(\Symfony\Component\HttpFoundation\Request $request,$key = 1) {
+        function newAction(\Symfony\Component\HttpFoundation\Request $request,$key = 1) {
         if(!$this->isValid(3))
         {
             echo "no access";
